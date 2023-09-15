02e805d97a9a8388fb8310196d37f34d44bd9cef
Luís Borges Araújo <42938615+Lp0lp@users.noreply.github.com>
2023-09-15 16:55:58 +0200

    Tweaking build

--- GoMartini/__init__.py
+++ GoMartini/__init__.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("GoMartini")
+except PackageNotFoundError:
+    # If the package is not installed, don't add __version__
+    pass
